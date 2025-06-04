e33cf97c2535d6e8ee2c6e0ffab65071802d941a
Manvel05 <abrah802@umn.edu>
2025-06-04 12:25:30 -0500

    Update MyEnergyAnalysis_module.cc

--- myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
+++ myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
@@ -949,7 +949,7 @@ namespace lar {
 
   // 2) Loop over each particle
   for (int l=0; l<fSim_nParticles; l++) {
-    const simb::MCParticle& particleVec = *(SimParticles[l]);
+    
    
 
      /*   fSim_start_4position.push_back(positionStart.X());
@@ -968,7 +968,7 @@ namespace lar {
 		fSim_end_4mommenta.push_back(momentumEnd.Py());
 		fSim_end_4mommenta.push_back(momentumEnd.Pz());
 		fSim_end_4mommenta.push_back(momentumEnd.E()); */
-    //}
+    
         // loop over every trajectory point, compare to geometry,
     // pull out E, subtract m, and do something with KE
     size_t Ntraj = particleVec.NumberTrajectoryPoints();
