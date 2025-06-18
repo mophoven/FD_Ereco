beb65461eea7be96ba8c584f2c260b5a1251d81a
Manvel05 <abrah802@umn.edu>
2025-06-18 11:27:03 -0500

    Update MyEnergyAnalysis_module.cc

--- myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
+++ myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
@@ -991,11 +991,11 @@ namespace lar {
 
 	    const size_t Ntrajpoints = particleVec.NumberTrajectoryPoints();
 
-  	  const int last = Ntrajpoints - 1;
-	    const TLorentzVector& positionStart = particleVec.Position(0);
-	    const TLorentzVector& positionEnd = particleVec.Position(last);
-	    const TLorentzVector& momentumStart = particleVec.Momentum(0);
-	    const TLorentzVector& momentumEnd = particleVec.Momentum(last);
+  	  //const int last = Ntrajpoints - 1;
+	    //const TLorentzVector& positionStart = particleVec.Position(0);
+	    //const TLorentzVector& positionEnd = particleVec.Position(last);
+	    //const TLorentzVector& momentumStart = particleVec.Momentum(0);
+	    //const TLorentzVector& momentumEnd = particleVec.Momentum(last);
       // New stuff
       double fXmin, fXmax, fYmin, fYmax, fZmin, fZmax;
       auto const& geom = *fGeometryService;
