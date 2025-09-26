3d614fad9d85e497f38af188612440889aea1635
Manvel05 <abrah802@umn.edu>
2025-09-26 11:49:39 -0500

    Update MyEnergyAnalysis_module.cc

--- myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
+++ myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
@@ -1054,17 +1054,20 @@ namespace lar
         double Z_MIN = 0.0, Z_MAX = 1300.0;
         const size_t Ntraj = particleVec.NumberTrajectoryPoints();
         bool hasEntered = false;
-        if (Ntraj > 0) {
+        if (localX >= X_MIN && localX <= X_MAX &&
+                localY >= Y_MIN && localY <= Y_MAX &&
+                localZ >= Z_MIN && localZ <= Z_MAX){
+        bool hasEntered = true;
+      }
+        if (!hasEntered && ipt > 0 ) {
           size_t firstInside = Ntraj;  
            
-            if (localX >= X_MIN && localX <= X_MAX &&
-                localY >= Y_MIN && localY <= Y_MAX &&
-                localZ >= Z_MIN && localZ <= Z_MAX)
-            {
+            
+            
                 firstInside = ipt;
                 std::cout << "Found first inside at pt " << firstInside << "\n";
                 break;
-            }
+        }
           //}
 
           if (firstInside != Ntraj) {
