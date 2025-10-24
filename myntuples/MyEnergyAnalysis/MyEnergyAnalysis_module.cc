a9919fab146fe8e9581f956daa623bde310bedda
Manvel05 <abrah802@umn.edu>
2025-10-24 13:46:13 -0500

    Update MyEnergyAnalysis_module.cc

--- myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
+++ myntuples/MyEnergyAnalysis/MyEnergyAnalysis_module.cc
@@ -1118,7 +1118,8 @@ namespace lar
                 double dx = b.X() - a.X(), dy = b.Y() - a.Y(), dz = b.Z() - a.Z();
                 double ds = std::sqrt(dx * dx + dy * dy + dz * dz);
                 //std::cout << "seg " << i << "->" << (i + 1) << "  ds=" << ds << " cm\n";
-                double Sums += ds;
+                double Sums = 0.0;
+                Sums += ds;
                 // sum all ds to get total track length inside the fiducial volume
               }
               std::cout<<Sums<<std::endl;
